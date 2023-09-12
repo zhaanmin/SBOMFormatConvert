bc99e9c0100f7697d36643529d9168ff328db1ac
zhaanmin <zhaanmin@126.com>
2023-09-12 13:45:55 +0800

    Add files via upload

---
+++ src/main/java/com/philips/sfc/SbomFormatConvertApplication.java
@@ -0,0 +1,13 @@
+package com.philips.sfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbomFormatConvertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbomFormatConvertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/philips/sfc/SbomFormatConvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philips.sfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbomFormatConvertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
